92aef1c9680e8608ca07b662e76005a08194a793
Kubek Monika <monika.kubek@tutanota.com>
2022-07-31 15:07:49 +0000

    Add fct_orders.sql table

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,7 @@
+select
+    orders.order_id
+    orders.customer_id
+    payments.amount
+from {{ ref('stg_payments') }} as payments
+    inner join {{ ref('stg_orders') }} as orders
+        on payments.order_id = orders.order_id
