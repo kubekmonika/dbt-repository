e80474be13d5b1bdf87f26607db1959208ced518
Kubek Monika <monika.kubek@tutanota.com>
2022-07-31 15:09:19 +0000

    Fix formatting issue

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,6 +1,6 @@
 select
-    orders.order_id
-    orders.customer_id
+    orders.order_id,
+    orders.customer_id,
     payments.amount
 from {{ ref('stg_payments') }} as payments
     inner join {{ ref('stg_orders') }} as orders
