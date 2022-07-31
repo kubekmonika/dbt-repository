7586a839ef6274eb99c8f33b4583074726eca5ac
Kubek Monika <monika.kubek@tutanota.com>
2022-07-31 15:37:36 +0000

    Refactor

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,32 +6,23 @@ with customer_orders as (
         min(orders.order_date) as first_order_date,
         max(orders.order_date) as most_recent_order_date,
         count(orders.order_id) as number_of_orders,
-        sum(payments.amount) as lifetime_value
+        sum(orders.amount) as lifetime_value
 
-    from {{ ref('stg_orders')}} as orders
-        left join {{ ref('stg_payments') }} as payments
-            on payments.order_id = orders.order_id
+    from {{ ref('fct_orders')}} as orders
 
     group by 1
 
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
-
-    from {{ ref('stg_customers') }} as customers
-
-    left join customer_orders using (customer_id)
-
 )
 
-select * from final
+select
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name,
+    customer_orders.first_order_date,
+    customer_orders.most_recent_order_date,
+    coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+    coalesce(customer_orders.lifetime_value, 0) as lifetime_value
+
+from {{ ref('stg_customers') }} as customers
+
+left join customer_orders using (customer_id)
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
-    created
+    amount / 100 as amount,
+    created as created_at
 
 from dbt-tutorial.stripe.payment
