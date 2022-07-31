5abc131b3a2d3b0b87316b7b93be0b240222aee5
Kubek Monika <monika.kubek@tutanota.com>
2022-07-31 15:17:46 +0000

    Add lifetime value to customers

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,13 +1,16 @@
 with customer_orders as (
 
     select
-        customer_id,
+        orders.customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(payments.amount) as lifetime_value
 
-    from {{ ref('stg_orders')}}
+    from {{ ref('stg_orders')}} as orders
+        left join {{ ref('stg_payments') }} as payments
+            on payments.order_id = orders.order_id
 
     group by 1
 
@@ -22,7 +25,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from {{ ref('stg_customers') }} as customers
 
